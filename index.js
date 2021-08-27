db0f2b9ae93a111d39d295b3b751b847db75d9a4
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-27 19:36:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const {add,sub,name,mul,div}=require('./operation');
+
+
+console.log(add(10,5));
+console.log(sub(30,5));
+console.log(mul(10,5));
+console.log(div(50,5));
+console.log(name);
---
+++ operation.js
@@ -0,0 +1,16 @@
+const add=(a,b)=>{
+    return a+b;
+};
+const sub=(a,b)=>{
+    return a-b;
+};
+const mul=(a,b)=>{
+    return a*b;
+};
+const div=(a,b)=>{
+    return a/b;
+};
+
+const name="Md Adil Alam";
+
+module.exports={add,sub,name,mul,div};
